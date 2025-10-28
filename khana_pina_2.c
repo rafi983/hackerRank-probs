80d20c1e26a2ad807d24d934cfeb5a57a94b949a
rafi zaman <rafiirfan211@gmail.com>
2025-10-28 17:38:08 +0600

    first commit

---
+++ khana_pina_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int M;
+
+    scanf("%d", &M);
+
+    if (M % 3 == 0) {
+        printf("Yes\n");
+    } else {
+        printf("No\n");
+    }
+
+    return 0;
+}
+
---
+++ salami.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int X, Y, Z;
+
+    scanf("%d %d %d", &X, &Y, &Z);
+
+    int total = X + Y + Z;
+    printf("%d\n", total);
+
+    return 0;
+}
